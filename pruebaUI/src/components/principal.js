1975e858a1de287cc1a28da68ae19314b1dc40ea
Soymichel <mtraatabladaa94@gmail.com>
2019-02-21 15:57:44 -0600

    Corrigiendo Bugs

--- pruebaUI/src/components/principal.js
+++ pruebaUI/src/components/principal.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import TestStyle from './testStyle'
+import TestPlatform from './testPlatform'
+import Crud from './../containers/crudContainer'
 
 const Principal = (props) => {
 
     return (
-        <TestStyle />
+        <TestPlatform />
     )
 
 }
